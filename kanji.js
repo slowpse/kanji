969aa0c87d9fb8b446543a02f1902866a3e753bf
slowpse <116038487+slowpse@users.noreply.github.com>
2025-07-25 09:49:42 +0800

    Create kanji.js

---
+++ kanji.js
@@ -0,0 +1,12 @@
+  const kanjiData = [
+    { kanji: "水", hiragana: "みず", english: "water" },
+    { kanji: "火", hiragana: "ひ", english: "fire" },
+    { kanji: "木", hiragana: "き", english: "tree" },
+    { kanji: "空", hiragana: "そら", english: "sky" },
+    { kanji: "山", hiragana: "やま", english: "mountain" },
+    { kanji: "風", hiragana: "かぜ", english: "wind" },
+    { kanji: "金", hiragana: "きん", english: "gold" },
+    { kanji: "土", hiragana: "つち", english: "earth" },
+    { kanji: "日", hiragana: "ひ", english: "sun" },
+    { kanji: "月", hiragana: "つき", english: "moon" }
+  ];
